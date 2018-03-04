920a077963db480f735a142390faffb4d35ca3f9
wolf1996 <ksgiv37@gmail.com>
2018-03-04 18:55:54 +0300

    Добавлен шаблон для data service

---
+++ python/data/data/app.py
@@ -0,0 +1,23 @@
+import data_pb2_grpc
+import data_pb2
+from concurrent import futures
+import grpc
+import time
+
+class DataServiceServ(data_pb2_grpc.DataServiceServicer):
+    def GetSensorData(self, request, context):
+        return 
+        
+def main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    data_pb2_grpc.add_DataServiceServicer_to_server(DataServiceServ(), server)
+    server.add_insecure_port('[::]:5052')
+    server.start()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    main()
