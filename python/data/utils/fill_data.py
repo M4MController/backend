70ba3d07126619af4ebbb2274b572775a685e253
wolf1996 <ksgiv37@gmail.com>
2018-10-06 22:19:50 +0300

    Добавить генерацию случайных данных

--- python/data/utils/fill_data.py
+++ python/data/utils/fill_data.py
@@ -1,13 +1,33 @@
 from pymongo import MongoClient
+from data_model.sensor_data import SensorDataModel
+import random
+from datetime import datetime
+from datetime import timedelta
+
 
 def main():
-    client = MongoClient('mongodb://localhost:27017/')
-    coll = client['sensors_data']['1']
-    coll.insert({
-        'value': 1,
-        'timestamp': 2,
-        'hash': 'adasxzscadqw',
-    })
+    time_delta = timedelta(days=-1)
+    maxdata = 99999
+    data_delta = 100
+    data_random = 10
+    num = 730
+    client = MongoClient('mongodb://192.168.39.236:30261/')
+    #client = MongoClient('mongodb://localhost:30261/')
+    data_model = SensorDataModel(client)
+    curr_data = maxdata
+    ts = datetime.now()
+    for i in range(0, num):
+        curr_delta = data_delta + random.randint(-data_random, data_random)
+        curr_data -= curr_delta
+        ts += time_delta
+        data_template = dict(
+            controller_mac="6b:45:cd:97:48:48",
+            sensor_id=1,
+            value=curr_data,
+            hash="HASH",
+            timestamp=ts.strftime('%Y-%m-%dT%H:%M:%S'),
+        )
+        data_model.insert_data(data_template)
 
 
 if __name__ == '__main__':
